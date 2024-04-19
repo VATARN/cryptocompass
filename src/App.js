c7e9b46908e4cf6073730c2d920bf75dd99d2e6c
Arnav Vats <arnav.vats@utexas.edu>
2024-04-19 05:45:15 -0500

    Layout setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import './App.css';
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Coin from "./Pages/coin";
+import Compare from "./Pages/compare";
+import Dashboard from "./Pages/dashboard";
+import Home from "./Pages/home";
+import Watchlist from "./Pages/watchlist";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+      },
+    },
+  });
+
   return (
     <div className="App">
+      <ToastContainer />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/coin/:id" element={<Coin />} />
+            <Route path="/compare" element={<Compare />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
